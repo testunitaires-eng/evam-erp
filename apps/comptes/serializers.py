a0619d335621c665ad10f839d62620328f050181
bacleche <schadracmoualou@gmail.com>
2026-08-31 02:33:00 +0200

    changement des hashage

--- apps/comptes/serializers.py
+++ apps/comptes/serializers.py
@@ -11,9 +11,29 @@ from rest_framework import serializers
 from . import models
 
 class UtilisateurSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
     class Meta:
         model = models.Utilisateur
         fields = "__all__"
+
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = models.Utilisateur(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 
 
 class MatriceDroitSerializer(serializers.ModelSerializer):
